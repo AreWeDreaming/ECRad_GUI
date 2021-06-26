e8c063b4cad372579ccf7ad5bca12ca77226c858
Severin Denk <severin.denk@ipp.mpg.de>
2021-06-26 23:08:56 +0200

    AUG fix 	modified:   ECRad_GUI_Calibration_Suite.py 	modified:   ECRad_GUI_PlotPanel.py 	modified:   ECRad_GUI_ScenarioPanel.py 	modified:   launch_ECRad_GUI.sh 	modified:   launch_ECRad_GUI.tcsh

--- launch_ECRad_GUI.sh
+++ launch_ECRad_GUI.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
-if [ $SYS == "amd64_sles15" ]
+if [[ $HOSTNAME == *"mpg"* ]]; then
   then
   module purge
   module load intel
   module load mkl
   module load texlive
-  module load anaconda/3/2018.12
+  module load anaconda/3/2020.02
   module load git
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MKLROOT/lib/intel64/
+else if [[ $HOSTNAME == *"mit"* ]]; then
+  # User specific aliases and functions
+  module use /home/software/psfc/modulefiles/
+  module load psfc/config
+  module load anaconda3/2020.11
+  module load psfc/netcdf/intel-17/4.4.1.1
+  module load intel
+  module load psfc/pgplot/5.2.2
+  module load texlive
+  module load engaging/git
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+  __conda_setup="$('/home/software/anaconda3/2020.11/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+      eval "$__conda_setup"
+  else
+    if [ -f "/home/software/anaconda3/2020.11/etc/profile.d/conda.sh" ]; then
+        . "/home/software/anaconda3/2020.11/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/software/anaconda3/2020.11/bin:$PATH"
+    fi
+  fi
+  unset __conda_setup
+  # <<< conda initialize <<<
+  conda activate ECRad_conda
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MKLROOT/lib/intel64/
+else if [[ $HOSTNAME == *"iter"* ]]; then
+  module purge
+  module load IMAS
+  module load texlive
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MKLROOT/lib/intel64/
 fi
 rm id
 git rev-parse HEAD > id
