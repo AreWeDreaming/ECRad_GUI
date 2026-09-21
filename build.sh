a0e9eb29b9d1194a66413cbf49c259ea982b5e23
arewedreaming <denks@fusion.gat.com>
2026-09-21 12:57:15 -0700

    Move feedstock to ECRad super repo

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 -m build -n -x
-python3 -m pip install --no-deps .
-git rev-parse HEAD > id
-# make USE_3D=True DEBUG=True
-# make USE_3D=True
-# make USE_3D=True DEBUG=True OPEN_MP=True
-# make USE_3D=True OPEN_MP=True
